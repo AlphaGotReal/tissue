8d990252e82a69f9fc7ce777597207bb3461fdd5
AlhpaGotReal <subhampatra2411@gmail.com>
2025-11-18 21:46:40 +0530

    plugin works

--- src/main/java/local/paper/TissuePaper.java
+++ src/main/java/local/paper/TissuePaper.java
@@ -1,4 +1,19 @@
 package local.paper;
 
-public class TissuePaper {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TissuePaper extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        getLogger().severe("Plugin loaded");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().severe("Plugin enabled");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().severe("Plugin disabled");
+    }
 }
