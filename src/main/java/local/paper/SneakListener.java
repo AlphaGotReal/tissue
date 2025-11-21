e8e8e1cdb669000905b6fb3bf3cd0fe60767a33e
AlhpaGotReal <subhampatra2411@gmail.com>
2025-11-21 13:16:02 +0530

    fix some math

--- src/main/java/local/paper/SneakListener.java
+++ src/main/java/local/paper/SneakListener.java
@@ -1,16 +1,30 @@
 package local.paper;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class SneakListener implements Listener {
+//    @EventHandler
+//    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
+//        Player player = event.getPlayer();
+//        if (player.isSneaking()) {
+//            player.setHealth(20);
+//        }
+//    }
+
     @EventHandler
-    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
+    public void onPlayerSneak(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
-        if (player.isSneaking()) {
-            player.setHealth(20);
-        }
+        World world = player.getWorld();
+        Location location = player.getLocation();
+        Block block = world.getBlockAt(new Location(world, location.x(), location.y()-1, location.z()));
+        player.getInventory().addItem(new ItemStack(block.getType()));
     }
+
 }
