ac29a5b0f3725ebcdc67ccf91ab2f168b951286e
AlhpaGotReal <subhampatra2411@gmail.com>
2026-02-01 07:53:04 +0530

    added sneaklistener

--- src/main/java/local/paper/SneakListener.java
+++ src/main/java/local/paper/SneakListener.java
@@ -6,6 +6,7 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -18,13 +19,19 @@ public class SneakListener implements Listener {
 //        }
 //    }
 
+//    @EventHandler
+//    public void onPlayerSneak(PlayerToggleSneakEvent event) {
+//        Player player = event.getPlayer();
+//        World world = player.getWorld();
+//        Location location = player.getLocation();
+//        Block block = world.getBlockAt(new Location(world, location.x(), location.y()-1, location.z()));
+//        player.getInventory().addItem(new ItemStack(block.getType()));
+//    }
+
     @EventHandler
     public void onPlayerSneak(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
-        World world = player.getWorld();
-        Location location = player.getLocation();
-        Block block = world.getBlockAt(new Location(world, location.x(), location.y()-1, location.z()));
-        player.getInventory().addItem(new ItemStack(block.getType()));
+        player.setMaxHealth(22);
     }
 
 }
